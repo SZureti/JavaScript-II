301361dfb95c1bd7e726c9778aeaf2fb4bf183e3
SAIPHER ZURETI <saipherzureti@gmail.com>
2019-06-11 11:05:58 -0500

    Initial commit

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,18 +1,29 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
-
+const lastName = 'Zureti';
+function greet() {
+  const firstName = 'Saipher';
+  alert('Hello ${firstName} ${lastName}, how are you?');
+}
+console.log(lastName);
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+const counter = () => { 
+  let count = 0;
+  return function (){
+    count = count + 1;
+    return count;
+  }
   // Return a function that when invoked increments and returns a counter variable.
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
-
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
